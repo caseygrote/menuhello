1f6567e78836d2242396586d77a3f57ddcda00c3
casey <caseygrote@gmail.com>
2014-06-10 14:09:38 -0400

    added icons and labels to assets.lua
    **some of the images were renamed

--- main.d
+++ main.d
@@ -1,133 +1,139 @@
 main.o: main.cpp \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/abi.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/abi/syscall.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/abi/types.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/abi/audio.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/abi/events.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/abi/vram.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/abi/asset.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/abi/elf.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/array.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/memory.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/macros.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/asset.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/asset/group.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/limits.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/math.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/asset/loader.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/asset/audio.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/asset/image.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/audio.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/cube.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/event.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/filesystem.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/metadata.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/motion.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/string.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/system.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/time.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/video.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/video/color.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/video/sprite.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/video/framebuffer.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/video/bg0rom.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/video/bg0.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/video/bg1.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/video/bg2.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/video/tilebuffer.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/menu.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/menu/types.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/menu/util.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/menu/public.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/menu/states.h \
-  C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/menu/events.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/syscall.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/types.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/audio.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/events.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/vram.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/asset.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/elf.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/array.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/memory.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/macros.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/asset.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/asset/group.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/limits.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/math.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/asset/loader.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/asset/audio.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/asset/image.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/audio.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/bluetooth.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/cube.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/event.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/filesystem.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/metadata.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/motion.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/string.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/system.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/time.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/usb.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/video.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/video/color.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/video/sprite.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/video/framebuffer.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/video/bg0rom.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/video/bg0.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/video/bg1.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/video/bg2.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/video/tilebuffer.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/menu.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/menu/types.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/menu/util.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/menu/public.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/menu/states.h \
+  C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/menu/events.h \
   assets.gen.h
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/abi.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/abi/syscall.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/syscall.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/abi/types.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/types.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/abi/audio.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/audio.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/abi/events.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/events.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/abi/vram.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/vram.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/abi/asset.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/asset.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/abi/elf.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/abi/elf.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/array.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/array.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/memory.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/memory.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/macros.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/macros.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/asset.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/asset.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/asset/group.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/asset/group.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/limits.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/limits.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/math.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/math.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/asset/loader.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/asset/loader.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/asset/audio.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/asset/audio.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/asset/image.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/asset/image.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/audio.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/audio.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/cube.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/bluetooth.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/event.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/cube.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/filesystem.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/event.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/metadata.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/filesystem.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/motion.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/metadata.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/string.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/motion.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/system.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/string.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/time.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/system.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/video.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/time.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/video/color.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/usb.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/video/sprite.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/video.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/video/framebuffer.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/video/color.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/video/bg0rom.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/video/sprite.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/video/bg0.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/video/framebuffer.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/video/bg1.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/video/bg0rom.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/video/bg2.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/video/bg0.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/video/tilebuffer.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/video/bg1.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/menu.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/video/bg2.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/menu/types.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/video/tilebuffer.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/menu/util.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/menu.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/menu/public.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/menu/types.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/menu/states.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/menu/util.h:
 
-C:\Users\beast-jr\Desktop\sifteo-sdk-windows-v1.0.0-71-g0792e5f\/include\sifteo/menu/events.h:
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/menu/public.h:
+
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/menu/states.h:
+
+C:\Users\beast-jr\Desktop\TABULA_SIFTEO\sifteo-sdk-windows-v1.1.0-371-gaf55784\/include\sifteo/menu/events.h:
 
 assets.gen.h:
